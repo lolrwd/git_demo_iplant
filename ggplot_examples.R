1741510c82ed8cd70230b25598bf871a81b54b61
lolrwd <ian.s.kaufer@gmail.com>
2015-02-22 14:44:56 -0700

    More ggplot examples

--- ggplot_examples.R
+++ ggplot_examples.R
@@ -1,3 +1,26 @@
 # ggplot leanring script
 # Ian Kaufer ikaufer@email.arizona.edu
 # 22-Feb 2015
+
+install.packages("ggplot2", dependencies=TRUE)
+install.packages("plyr")
+install.packages("ggthemes")
+install.packages("reshape2")
+
+library("ggplot2")
+library("reshape2")
+library("ggthemes")
+library("plyr")
+
+myplot <- ggplot(data = iris, aes(x=Sepal.Length, y=Sepal.Width, color=Species)) + 
+  geom_point(aes(shape=Species),size=4)
+myplot
+
+d2 <- diamonds[sample(1:dim(diamonds)[1], 1000), ]
+dim(diamonds)
+dim(diamonds)[1]
+
+ggplot(d2, aes(carat,price, color=color)) + geom_point()
+
+library("MASS")
+ggplot(birthwt, aes(factor(race),bwt)) + geom_boxplot()
